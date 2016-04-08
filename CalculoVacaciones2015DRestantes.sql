100f98766bf99c1d34e1d22b637ed6532edadbbd
santiago <bokeroncrc@gmail.com>
2016-04-08 17:17:26 +0200

    Scipt
    calculo dias restantes

---
+++ CalculoVacaciones2015DRestantes.sql
@@ -0,0 +1,16 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `vacacionesAntiguasSimpleMejorada`(anio int,empleadoId int)
+BEGIN     
+declare idPais int;
+
+SET			 SQL_SAFE_UPDATES = 0;
+UPDATE 		 vacaciones
+SET          diasExcedidos=NULL,
+             diasRestantes=NULL WHERE estado
+             IN ('Rechazadas','Canceladas');
+ 
+select   	idCalendario from calendarios limit 1 into idPais;
+update   	vacaciones  
+SET      	diasRestantes=(getVacacionesTotales(anio, empleadoId)-datediff(fechaFin,fechaInicio))+ (select portalempleado.countNumFestivosRango2(idPais, fechaInicio, fechaFin))-1                                       
+where    	idEmpleado=empleadoId;
+
+end
