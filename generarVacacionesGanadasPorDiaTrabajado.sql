3bf48e118cc698ece4128f62a9f1ecc8f708845c
santiago <bokeroncrc@gmail.com>
2016-04-11 17:09:39 +0200

    Update generarVacacionesGanadasPorDiaTrabajado.sql

--- generarVacacionesGanadasPorDiaTrabajado.sql
+++ generarVacacionesGanadasPorDiaTrabajado.sql
@@ -4,11 +4,21 @@ declare diasGen int;
 declare tipoVac varchar(50);
 select tipoCalculoVacaciones into tipoVac from empleados WHERE idEmpleado = idE;
 begin
- IF tipoVac ='Laborables' THEN
+IF tipoVac ='Laborables' THEN
+ 
+        if fAlta > '2016-01-01' then
+        set diasGen = ((((datediff(curdate(),fAlta)+1)-(datediff(curdate(),fAlta)+1)*31.23/100))/30)*1.83;
+        else
         set diasGen = ((((datediff(curdate(),'2016-01-01')+1)-(datediff(curdate(),'2016-01-01')+1)*31.23/100))/30)*1.83;
- else
+        end if;
+ else    
+        if fAlta> '2016-01-01' then
         set diasGen=((((datediff(curdate(),'2016-01-01')+1)-(datediff(curdate(),'2016-01-01')+1)*31.23/100))/30)*2.5;
+        else
+        set diasGen=((((datediff(curdate(),'2016-01-01')+1)-(datediff(curdate(),'2016-01-01')+1)*31.23/100))/30)*2.5;
+        end if;
 END if;
+
 end;
 return diasGen;
 end
