0c43a9fd684e12f9472aecd2ab117894d8391a70
santiago <bokeroncrc@gmail.com>
2016-04-11 17:08:25 +0200

    Ampliacion procedure dias generados

--- generarVacacionesGanadasPorDiaTrabajado.sql
+++ generarVacacionesGanadasPorDiaTrabajado.sql
@@ -1,13 +1,24 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `generarVacacionesGanadasPorDiaTrabajado`(`idE`int) RETURNS int(200)
+CREATE DEFINER=`root`@`localhost` FUNCTION `generarVacacionesPorDiaTrabajado`(`idE`int) RETURNS int(200)
 BEGIN
 declare diasGen int;
+declare fAlta date;
 declare tipoVac varchar(50);
 select tipoCalculoVacaciones into tipoVac from empleados WHERE idEmpleado = idE;
+select fechaAlta into fAlta from empleados where idEmpleado=idE;
 begin
  IF tipoVac ='Laborables' THEN
+ 
+        if fAlta > '2016-01-01' then
+        set diasGen = ((((datediff(curdate(),fAlta)+1)-(datediff(curdate(),fAlta)+1)*31.23/100))/30)*1.83;
+        else
         set diasGen = ((((datediff(curdate(),'2016-01-01')+1)-(datediff(curdate(),'2016-01-01')+1)*31.23/100))/30)*1.83;
- else
+        end if;
+ else    
+        if fAlta> '2016-01-01' then
         set diasGen=((((datediff(curdate(),'2016-01-01')+1)-(datediff(curdate(),'2016-01-01')+1)*31.23/100))/30)*2.5;
+        else
+        set diasGen=((((datediff(curdate(),'2016-01-01')+1)-(datediff(curdate(),'2016-01-01')+1)*31.23/100))/30)*2.5;
+        end if;
 END if;
 end;
 return diasGen;
